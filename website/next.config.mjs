711b0e6bee648d5c0b98b01ac6e4f7c893483d62
Bhushan Moglade <3489df14-9935-461c-b974-5760c1194e84>
2026-04-29 12:41:50 +0000

    Autogenerated commit message at Apr 29, 2026, 12:41 PM UTC

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,12 +1,15 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "*.supabase.co",
         pathname: "/storage/v1/object/public/**",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
       },
     ],
   },
